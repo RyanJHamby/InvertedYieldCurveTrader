8fd2db1f7f81e397898e36b397ad702359239798
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2023-08-27 16:16:08 -0400

    Add main.cpp to set up repo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InvertedYieldCurveTrader
+//
+//  Created by Ryan Hamby on 8/27/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
