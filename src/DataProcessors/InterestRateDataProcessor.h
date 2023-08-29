684a3eb84c7ca14a452d41a3f34bd9c53dfd017a
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2023-08-29 08:09:09 -0400

    Add basic header files and folders

---
+++ src/DataProcessors/InterestRateDataProcessor.h
@@ -0,0 +1,12 @@
+//
+//  InterestRateDataProcessor.h
+//  InvertedYieldCurveTrader
+//
+//  Created by Ryan Hamby on 8/29/23.
+//
+
+#ifndef InterestRateDataProcessor_h
+#define InterestRateDataProcessor_h
+
+
+#endif /* InterestRateDataProcessor_h */
---
+++ src/DataProcessors/InvertedYieldDataProcessor.h
@@ -0,0 +1,12 @@
+//
+//  InvertedYieldDataProcessor.h
+//  InvertedYieldCurveTrader
+//
+//  Created by Ryan Hamby on 8/29/23.
+//
+
+#ifndef InvertedYieldDataProcessor_h
+#define InvertedYieldDataProcessor_h
+
+
+#endif /* InvertedYieldDataProcessor_h */
