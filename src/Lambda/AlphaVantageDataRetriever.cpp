f8f8aea3b33ae2253415e8343734d42981dfe589
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2023-09-19 09:09:02 -0400

    Add AlphaVantageDataRetriever for fetching data from stock market API

---
+++ src/Lambda/AlphaVantageDataRetriever.cpp
@@ -0,0 +1,8 @@
+//
+//  AlphaVantageDataRetriever.cpp
+//  InvertedYieldCurveTrader
+//
+//  Created by Ryan Hamby on 9/19/23.
+//
+
+#include "AlphaVantageDataRetriever.hpp"
---
+++ src/Lambda/AlphaVantageDataRetriever.hpp
@@ -0,0 +1,13 @@
+//
+//  AlphaVantageDataRetriever.hpp
+//  InvertedYieldCurveTrader
+//
+//  Created by Ryan Hamby on 9/19/23.
+//
+
+#ifndef AlphaVantageDataRetriever_hpp
+#define AlphaVantageDataRetriever_hpp
+
+#include <stdio.h>
+
+#endif /* AlphaVantageDataRetriever_hpp */
