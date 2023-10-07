e6b191be46da4a39ebea936ea2ecf6609c9364d2
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2023-10-06 21:13:08 -0400

    Fix bug for missing data in InvertedYieldStatsCalculator

--- src/DataProcessors/InvertedYieldStatsCalculator.cpp
+++ src/DataProcessors/InvertedYieldStatsCalculator.cpp
@@ -16,16 +16,21 @@ void InvertedYieldStatsCalculator::setData(const std::string& jsonData10Year, co
         nlohmann::json inflationData2Year = nlohmann::json::parse(jsonData2Year);
 
         std::vector<double> values;
-        for (int i = 0; i < inflationData10Year["data"].size(); ++i) {
+        for (int i = 0; i < 1000; ++i) {
             auto entry10Year = inflationData10Year["data"][i];
             auto entry2Year = inflationData2Year["data"][i];
+                        
+            if (entry10Year["value"] != "." && entry2Year["value"] != ".") {
+                //  Convert the "value" field from string to double and add it to the vector
+                double value10Year = std::stod(entry10Year["value"].get<std::string>());
+                double value2Year = std::stod(entry2Year["value"].get<std::string>());
             
-            // Convert the "value" field from string to double and add it to the vector
-            double value10Year = double(entry10Year["value"]);
-            double value2Year = double(entry2Year["value"]);
-            
-            // Inverted Yield = 10 year yield - 2 year yield
-            values.push_back(value10Year - value2Year);
+                // Inverted Yield = 10 year yield - 2 year yield
+                values.push_back(value10Year - value2Year);
+            } else {
+                // Maintain previous day for missing data, and maintain day ordering
+                values.push_back(values.back());
+            }
         }
         
         data = values;
