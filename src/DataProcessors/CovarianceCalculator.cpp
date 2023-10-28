c902ff4f71de03c6fc78b991c497035c3129b0ba
RyanJHamby <68158512+RyanJHamby@users.noreply.github.com>
2023-10-28 17:33:28 -0400

    Add covariance calculator class and clean up values of yield and inflation data processors

---
+++ src/DataProcessors/CovarianceCalculator.cpp
@@ -0,0 +1,8 @@
+//
+//  CovarianceCalculator.cpp
+//  InvertedYieldCurveTrader
+//
+//  Created by Ryan Hamby on 10/28/23.
+//
+
+#include "CovarianceCalculator.hpp"
---
+++ src/DataProcessors/CovarianceCalculator.hpp
@@ -0,0 +1,13 @@
+//
+//  CovarianceCalculator.hpp
+//  InvertedYieldCurveTrader
+//
+//  Created by Ryan Hamby on 10/28/23.
+//
+
+#ifndef CovarianceCalculator_hpp
+#define CovarianceCalculator_hpp
+
+#include <stdio.h>
+
+#endif /* CovarianceCalculator_hpp */
